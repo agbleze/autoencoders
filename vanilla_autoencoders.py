be26c6f2e64d20283e69891e92be884387b02b3f
linus <lagbleze@st.ug.edu.gh>
2024-02-07 01:07:36 +0100

    Update vanilla_autoencoders.py

--- vanilla_autoencoders.py
+++ vanilla_autoencoders.py
@@ -64,6 +64,34 @@ model = AutoEncoder(3).to(device)
 summary(model, (1,28,28))
 
 
+#%% define training func
+def train_batch(input, model, criterion, optimizer):
+    model.train()
+    optimizer.zero_grad()
+    output = model(input)
+    loss = criterion(output, input)
+    loss.backward()
+    optimizer.step()
+    return loss
+
+#%% define validation func
+@torch.no_grad()
+def validate_batch(input, model, criterion):
+    model.eval()
+    output = model(input)
+    loss = criterion(output, input)
+    return loss
+
+
+#%% initialize model, loss criterion, optimizer
+model = AutoEncoder(3).to(device)
+criterion = nn.MSELoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=1e-5)
+
+
+#%% train the model
+num_epochs = 5
+log = Report(num_epochs)
 
 
 
