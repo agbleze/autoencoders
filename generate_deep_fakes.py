d0ce6faae25b77611d1703f97fc359a87c571b82
linus <lagbleze@st.ug.edu.gh>
2024-02-12 23:12:34 +0100

    Update generate_deep_fakes.py

--- generate_deep_fakes.py
+++ generate_deep_fakes.py
@@ -76,4 +76,129 @@ for i in next(iter(x)):
     subplots(i[:8], nc=4, sz=(4,2))
 
 
+# %%  define convolution and upscaling, reshaping
+def _ConvLayer(input_features, output_features):
+    return nn.Sequential(nn.Conv2d(input_features, output_features, kernel_size=5, stride=2, padding=2),
+                         nn.LeakyReLU(0.1, inplace=True)
+                         )
+    
+def _UpScale(input_features, output_features):
+    return nn.Sequential(nn.ConvTranspose2d(input_features, output_features, kernel_size=2, stride=2, padding=0),
+                         nn.LeakyReLU(0.1, inplace=True)
+                         )
+    
+class Reshape(nn.Module):
+    def forward(self, input):
+        output = input.view(-1, 1024, 4, 4)
+        return output
+    
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()    
+        self.encoder = nn.Sequential(
+            _ConvLayer(3, 128),
+            _ConvLayer(128, 256),
+            _ConvLayer(256, 512),
+            _ConvLayer(512, 1024),
+            nn.Flatten(),
+            nn.Linear(1024 * 4 * 4, 1024),
+            nn.Linear(1024, 1024 * 4 *4),
+            Reshape(),
+            _UpScale(1024, 512),
+        )
+        
+        self.decoder_A = nn.Sequential(
+                        _UpScale(512, 256),
+                        _UpScale(256, 128),
+                        _UpScale(128, 64),
+                        nn.Conv2d(64, 3, kernel_size=3, padding=1),
+                        nn.Sigmoid(),
+                    )
+        
+        self.decoder_B = nn.Sequential(
+                    _UpScale(512, 256),
+                    _UpScale(256, 128),
+                    _UpScale(128, 64),
+                    nn.Conv2d(64, 3, kernel_size=3, padding=1),
+                    nn.Sigmoid()
+                    
+                )
+        
+    def forward(self, x, select="A"):
+        if select == "A":
+            out = self.encoder(x)
+            out = self.decoder_A(out)
+        else:
+            out = self.encoder(x)
+            out = self.decoder_B(out)
+        return out
+
+#%%
+from torchsummary import summary
+model = Autoencoder()
+summary(model, torch.zeros(32, 3, 64, 64), "A")
+
+# %%  ## train_batch
+
+def train_batch(model, data, criterion, optimizers):
+    optA, optB = optimizers
+    optA.zero_grad()
+    optB.zero_grad()
+    imgA, imgB, targetA, targetB = data
+    _imgA, _imgB = model(imgA, "A"), model(imgB, "B")
+    lossA = criterion(_imgA, targetA)
+    lossB = criterion(_imgB, targetB)
+    lossA.backward()
+    lossB.backward()
+    
+    optA.step()
+    optB.step()
+    return lossA.item(), lossB.item()
+
+
+
+#%%
+model = Autoencoder().to(device)
+dataset = ImageDataset(Glob("cropped_faces_personA"), Glob("cropped_faces_personB")
+                       )
+dataloader = DataLoader(dataset, 32, collate_fn=dataset.collate_fn)
+
+optimizers = optim.Adam([{"params": model.encoder.parameters()},
+                         {"params": model.decoder_A.parameters()}],
+                        lr=5e-5, betas=(0.5, 0.999)), optim.Adam([{"params": model.encoder.parameters()}, 
+                        {"params": model.decoder_B.parameters()}],
+                       lr=5e-5, betas=(0.5, 0.999)
+                       )
+criterion = nn.L1Loss()
+
+
+#%% train model
+n_epochs = 1000
+log = Report(n_epochs)
+!mkdir checkpoint
+for ex in range(n_epochs):
+    N = len(dataloader)
+    for bx, data in enumerate(dataloader):
+        lossA, lossB = train_batch(model, data, criterion, optimizers)
+        log.record(ex+(1+bx)/N, lossA=lossA, lossB=lossB, end="\r")
+    log.report_avgs(ex+1)
+    if (ex+1)%100 == 0:
+        state = {"state": model.state_dict(),
+                 "epoch": ex
+                 }
+        torch.save(state, "./checkpoint/autoencoder.pth")
+        
+    if (ex+1) % 100 == 0:
+        bs = 5
+        a,b,A,B = data
+        line("A to B")
+        _a = model(a[:bs], "A")
+        _b = model(a[:bs], "B")
+        x = torch.cat([B[:bs], _a, _b])
+        subplots(x, nc=bs, figsize=(bs*2, 5))
+log.plot_epochs()
+        
+    
+
 # %%
